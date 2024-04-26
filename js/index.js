6115b952bea88da6cd4a4212b8131e7abdbd5366
kaikilucas <kaiki.lucasf@gmail.com>
2024-04-26 14:46:10 -0300

    Mudei alguns detalhes no CSS

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
 function resultado() {
-    d1 = document.getElementById("diametro").value
-    a1 = document.getElementById("altura").value
-    pi = 3.14
-    r1 = (d1 * d1 * pi * a1) / 4
-
-
-
-
-
-
-    ola = document.getElementById("aqui").innerHTML = r1.toFixed(3) + " Litros"
-
-
+  d1 = document.getElementById("diametro").value;
+  a1 = document.getElementById("altura").value;
+  pi = 3.14;
+  r1 = (d1 * d1 * pi * a1) / 4;
+  r2 = r1.toFixed(3);
+  ola = document.getElementById("aqui").innerHTML =
+    "O resultado é de " + r2 + " Litros";
 }
